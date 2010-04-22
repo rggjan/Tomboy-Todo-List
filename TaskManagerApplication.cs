fb65a03c2a8e8c0d0b1dbb0f619fc5c5ed89e6a8
gabster <walchg@student.ethz.ch>
2010-04-22 19:04:48 +0200

    Changed working example to some startup draft

---
+++ TaskManagerApplication.cs
@@ -0,0 +1,40 @@
+using System;
+using Mono.Unix;
+using Gtk;
+using Tomboy;
+
+namespace Tomboy.TaskManager
+{
+	class TaskManagerApplicationAddin : ApplicationAddin{
+		
+		private bool initialized;
+		
+		public override bool Initialized{
+			get{return initialized;}	
+		}
+		
+		public override void Shutdown(){
+			
+		}
+		
+		public override void Initialize(){
+			Tomboy.ActionManager.UI.AddUiFromString(@"
+			<ui>
+			<menubar name='MainWindowMenubar'>
+	    	<placeholder name='MainWindowMenuPlaceholder'>
+	      	<menu name='ToolsMenu' action='ToolsMenuAction'>
+	        <menuitem name='TaskManager' action='TaskManagerAction'/>
+	      	</menu>
+	    	</placeholder>
+	  		</menubar>
+			</ui> 
+			");
+			
+			ActionGroup group = new ActionGroup("TaskManager");
+			group.Add (new Gtk.ActionEntry [] {new Gtk.ActionEntry ("TaskManagerAction", null,"Test", null, null, null)});
+			
+			Tomboy.ActionManager.UI.InsertActionGroup(group,0);
+			initialized = true;
+		}
+	}
+}
