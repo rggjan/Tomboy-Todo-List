d1f3dfc7d28e7df874c8456a539a104244a165ad
Gabriel Walch <gabriel@gabriel-laptop.(none)>
2010-04-22 19:04:48 +0200

    Changed working example to some startup draft

---
+++ TaskManagerNote.cs
@@ -0,0 +1,36 @@
+using System;
+using Mono.Unix;
+using Gtk;
+using Tomboy;
+
+namespace Tomboy.TaskManager
+{
+	public class TaskManagerNoteAddin : NoteAddin
+	{
+		Gtk.MenuItem item;
+
+		public override void Initialize ()
+		{
+			item = new Gtk.MenuItem (Catalog.GetString ("Insert date and time"));
+			item.Activated += OnMenuItemActivated;
+			item.Show ();
+			AddPluginMenuItem (item);
+		}
+
+		public override void Shutdown ()
+		{
+			item.Activated -= OnMenuItemActivated;
+		}
+
+		public override void OnNoteOpened ()
+		{
+		}
+
+		void OnMenuItemActivated (object sender, EventArgs args)
+		{	
+			NoteBuffer buffer = Note.Buffer;
+			buffer.InsertAtCursor (Tomboy.ActionManager.UI.Ui);
+			
+		}
+	}
+}
