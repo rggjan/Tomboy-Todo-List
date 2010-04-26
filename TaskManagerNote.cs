7ead933ed3bd2a6b719b693f12e98457825243b0
Jan Rüegg <rggjan@gmail.com>
2010-04-26 19:33:07 +0200

    Added checkbox + strikethrough functionality... very basic... very ugly.. just testing :)

--- TaskManagerNote.cs
+++ TaskManagerNote.cs
@@ -11,10 +11,31 @@ namespace Tomboy.TaskManager
 
 		public override void Initialize ()
 		{
-			item = new Gtk.MenuItem (Catalog.GetString ("Insert date and time"));
+			
+        string styleMod =
+        @"style ""mystyle"" {
+         	#GtkCheckButton::indicator-spacing = 0
+         	#GtkCheckButton::focus-padding = 0
+         	#GtkCheckButton::focus-line-width = 2
+			#GtkCheckButton::indicator-size = 100
+      	}
+      widget ""*.tomboy-inline-checkbox"" style ""mystyle""";
+      
+      Gtk.Rc.ParseString (styleMod);
+		
+			item = new Gtk.MenuItem (Catalog.GetString ("Add TaskList"));
 			item.Activated += OnMenuItemActivated;
 			item.Show ();
 			AddPluginMenuItem (item);
+			
+/*			NoteTag tag;
+			tag = new NoteTag ("abctag");
+//tag.Justification = Gtk.Justification.Center;
+			tag.Scale = 10;
+			tag.CanUndo = true;
+			tag.CanGrow = true;
+			tag.CanSpellCheck = true;
+			Buffer.TagTable.Add (tag);*/
 		}
 
 		public override void Shutdown ()
@@ -27,14 +48,33 @@ namespace Tomboy.TaskManager
 		}
 
 		void OnMenuItemActivated (object sender, EventArgs args)
-		{	
-			NoteBuffer buffer = Note.Buffer;
-//	buffer.InsertAtCursor (Tomboy.ActionManager.UI.Ui);
-			buffer.InsertAtCursor ("This is a test[1]\n");
-			
-			
-			buffer.InsertAtCursor ("This is a test[2]");
+		{
 
+			InsertTaskList ();
+		}
+		
+		void ToggleCheckBox (object sender, EventArgs e)
+		{
+			NoteBuffer buffer = Note.Buffer;
+			buffer.ApplyTag ("strikethrough", buffer.StartIter, buffer.EndIter);
+		}
+		
+		void InsertTaskList ()
+		{
+			var checkbox = new Gtk.CheckButton ();
+			checkbox.Name = "tomboy-inline-checkbox";
+			
+			checkbox.Toggled += ToggleCheckBox;
+			
+			NoteBuffer buffer = Note.Buffer;
+			
+			Gtk.TextMark cursor = buffer.GetMark ("insert");
+			TextIter iter = buffer.GetIterAtMark (cursor);
+			Gtk.TextChildAnchor anchor = Buffer.CreateChildAnchor (ref iter);
+			Note.Window.Editor.AddChildAtAnchor (checkbox, anchor);
+			checkbox.Show ();
+			
+			//buffer.ApplyTag ("abctag", buffer.StartIter, buffer.EndIter);
 		}
 	}
 }
