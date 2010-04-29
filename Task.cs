d6d25aa84b068563cfa78a24af8a1b260f7d44ea
Gerd <mail@gerdzellweger.com>
2010-04-29 16:24:55 +0200

    first version for a task class

---
+++ Task.cs
@@ -0,0 +1,78 @@
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//   
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//   
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  
+// Authors:
+//       Jan Rüegg <rggjan@gmail.com>
+//       Gabriel Walch <walchg@student.ethz.ch>
+//       Gerd Zellweger <mail@gerdzellweger.com>
+// 
+
+using System;
+
+namespace Tomboy.TaskManager
+{
+
+
+	public class Task
+	{
+		/// <summary>
+		/// Description of the Task the user wrote in the Buffer
+		/// </summary>
+		public string Description
+		{ get; set; }
+		
+		/// <summary>
+		/// Priority for this Task
+		/// </summary>
+		public int Priority
+		{ get; set; }
+		// TODO find corresponding widget here
+		
+		/// <summary>
+		/// Is this task completed?
+		/// </summary>
+		public bool Completed { 
+			get {
+				return CheckBox.Active;
+			}
+			set {
+				CheckBox.Active = value;
+			}
+		}
+		/// <summary>
+		/// Corresponding Widget for Completed Tasks.
+		/// </summary>
+		private Gtk.CheckButton CheckBox {
+			get; set;
+		}
+		
+		/// <summary>
+		/// Date until the task should be completed.
+		/// </summary>
+		public DateTime DueDate 
+		{ get; set; }
+		private Gtk.Calendar DueDateWidget
+		{ get; set; }
+		
+
+		public Task ()
+		{
+		}
+	}
+}
