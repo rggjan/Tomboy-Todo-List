bdb080e129e497211dae02419d0fc5cfd8ed9572
Gerd Zellweger <mail@gerdzellweger.com>
2010-04-29 11:05:15 +0200

    playground initial

--- TaskManagerNote.cs
+++ TaskManagerNote.cs
@@ -11,31 +11,10 @@ namespace Tomboy.TaskManager
 
 		public override void Initialize ()
 		{
-			
-        string styleMod =
-        @"style ""mystyle"" {
-         	#GtkCheckButton::indicator-spacing = 0
-         	#GtkCheckButton::focus-padding = 0
-         	#GtkCheckButton::focus-line-width = 2
-			#GtkCheckButton::indicator-size = 100
-      	}
-      widget ""*.tomboy-inline-checkbox"" style ""mystyle""";
-      
-      Gtk.Rc.ParseString (styleMod);
-		
 			item = new Gtk.MenuItem (Catalog.GetString ("Add TaskList"));
 			item.Activated += OnMenuItemActivated;
 			item.Show ();
 			AddPluginMenuItem (item);
-			
-/*			NoteTag tag;
-			tag = new NoteTag ("abctag");
-//tag.Justification = Gtk.Justification.Center;
-			tag.Scale = 10;
-			tag.CanUndo = true;
-			tag.CanGrow = true;
-			tag.CanSpellCheck = true;
-			Buffer.TagTable.Add (tag);*/
 		}
 
 		public override void Shutdown ()
@@ -53,28 +32,9 @@ namespace Tomboy.TaskManager
 			InsertTaskList ();
 		}
 		
-		void ToggleCheckBox (object sender, EventArgs e)
-		{
-			NoteBuffer buffer = Note.Buffer;
-			buffer.ApplyTag ("strikethrough", buffer.StartIter, buffer.EndIter);
-		}
-		
 		void InsertTaskList ()
 		{
-			var checkbox = new Gtk.CheckButton ();
-			checkbox.Name = "tomboy-inline-checkbox";
-			
-			checkbox.Toggled += ToggleCheckBox;
-			
-			NoteBuffer buffer = Note.Buffer;
-			
-			Gtk.TextMark cursor = buffer.GetMark ("insert");
-			TextIter iter = buffer.GetIterAtMark (cursor);
-			Gtk.TextChildAnchor anchor = Buffer.CreateChildAnchor (ref iter);
-			Note.Window.Editor.AddChildAtAnchor (checkbox, anchor);
-			checkbox.Show ();
-			
-			//buffer.ApplyTag ("abctag", buffer.StartIter, buffer.EndIter);
+			TaskList taskList = new TaskList(Note);
 		}
 	}
 }
