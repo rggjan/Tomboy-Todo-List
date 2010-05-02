eee536dda500d2ae2834a0e89d43a7801a75f472
Jan Rüegg <rggjan@gmail.com>
2010-05-02 15:26:58 +0200

    Possible to add a Task with "[] + Enter"

--- TaskManagerNote.cs
+++ TaskManagerNote.cs
@@ -10,29 +10,11 @@ namespace Tomboy.TaskManager
 	{
 		Gtk.MenuItem item;
 
+		bool initialized = false;
+		bool new_task_needed = false;
+		
 		public override void Initialize ()
 		{
-			Logger.Debug ("Initializing TaskManager");
-			
-			string styleMod =
-				@"style ""mystyle"" {
-				#GtkCheckButton::indicator-spacing = 0
-				#GtkCheckButton::focus-padding = 0
-				#GtkCheckButton::focus-line-width = 2
-				#GtkCheckButton::indicator-size = 100
-				}
-				widget ""*.tomboy-inline-checkbox"" style ""mystyle""";
-			Gtk.Rc.ParseString (styleMod);
-		
-			item = new Gtk.MenuItem (Catalog.GetString ("Add TaskList"));
-			item.Activated += OnMenuItemActivated;
-			item.Show ();
-			AddPluginMenuItem (item);
-
-			// Register additional Tags
-			TaskListTag tlt = new TaskListTag ();
-			Note.TagTable.Add (tlt);
-			
 		}
 
 		public override void Shutdown ()
@@ -42,6 +24,74 @@ namespace Tomboy.TaskManager
 
 		public override void OnNoteOpened ()
 		{
+		
+			if (!initialized)
+			{
+				Logger.Debug ("Initializing TaskManager");
+				
+				/*string styleMod =
+					@"style ""mystyle"" {
+					#GtkCheckButton::indicator-spacing = 0
+					#GtkCheckButton::focus-padding = 0
+					#GtkCheckButton::focus-line-width = 2
+					#GtkCheckButton::indicator-size = 100
+					}
+					widget ""*.tomboy-inline-checkbox"" style ""mystyle""";
+				Gtk.Rc.ParseString (styleMod);*/
+			
+				item = new Gtk.MenuItem (Catalog.GetString ("Add TaskList"));
+				item.Activated += OnMenuItemActivated;
+				item.Show ();
+				AddPluginMenuItem (item);
+	
+				// Register additional Tags
+				TaskListTag tlt = new TaskListTag ();
+				Note.TagTable.Add (tlt);
+
+				Buffer.UserActionEnded += CheckIfNewTaskNeeded;
+				Buffer.InsertText += BufferInsertText;
+				
+				initialized = true;
+			}
+		}
+
+		void CheckIfNewTaskNeeded (object sender, System.EventArgs args)
+		{
+			if (new_task_needed == true)
+			{
+				Logger.Debug ("Adding a new Task");
+				
+				Gtk.TextIter end = Buffer.GetIterAtMark (Buffer.InsertMark);
+				Gtk.TextIter start = end;
+				start.BackwardChars (3);
+				Buffer.Delete (ref start, ref end);
+				
+				new TaskList (Note);
+
+				new_task_needed = false;
+			}
+		}
+	
+		void BufferInsertText (object o, Gtk.InsertTextArgs args)
+		{
+			if (args.Text == "\n") {
+				Gtk.TextIter end = args.Pos;
+				end.BackwardChar();
+				
+				Gtk.TextIter start = end;
+				start.LineOffset = 0;
+				
+				//Logger.Debug(Buffer.GetText(start, end, false));
+				
+				if(IsTextTodoItem(Buffer.GetText(start, end, false)))
+					new_task_needed = true;
+			}
+		}
+		
+		bool IsTextTodoItem (String text)
+		{
+			//Logger.Debug(text.Trim());
+			return text.Trim().Equals("[]");
 		}
 
 		void OnMenuItemActivated (object sender, EventArgs args)
