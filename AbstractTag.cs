c5dfc077e35fb2f0b54eaa3516cf3a911cfbdbec
Gabriel Walch <gabriel@gabriel-laptop.(none)>
2010-05-08 14:36:22 +0200

    ... And adding AbstractTag

---
+++ AbstractTag.cs
@@ -0,0 +1,54 @@
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//   
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//   
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  
+// Authors:
+//       Jan Rüegg <rggjan@gmail.com>
+//       Gabriel Walch <walchg@student.ethz.ch>
+//       Gerd Zellweger <mail@gerdzellweger.com>
+// 
+
+using System;
+using Tomboy;
+
+namespace Tomboy.TaskManager
+{
+	public abstract class AbstractTag : DynamicNoteTag
+	{
+		public abstract void bind(AttributedTask task);
+		
+		public AttributedTask AttachedTask{
+			get;
+			set;
+		}
+		
+		public void bindDefault(AttributedTask task){
+			AttachedTask = task;
+			
+			if (!Attributes.ContainsKey("Duedate"))
+				Attributes.Add ("Duedate", AttachedTask.DueDate.ToString ());
+			else
+				Attributes["Duedate"] = AttachedTask.DueDate.ToString();
+			
+			if (!Attributes.ContainsKey("Priority"))
+				Attributes.Add ("Priority", AttachedTask.Priority.ToString ());
+			else
+				Attributes["Priority"] = AttachedTask.Priority.ToString ();
+		}
+	}
+}
