ee99368950f176d97ab613a62254a62987eefb52
Gerd Zellweger <mail@gerdzellweger.com>
2010-05-18 11:55:52 +0200

    renamed testfile

--- Tests/DeserializeTests.cs
+++ Tests/DeserializeTests.cs
@@ -32,9 +32,12 @@ using Tomboy;
 namespace Tomboy.TaskManager.Tests
 {
 
-
+	/// <summary>
+	/// Tests that check if loading tasklists from notes xml files and setting up
+	/// the data structures is done correctly.
+	/// </summary>
 	[TestFixture()]
-	public class TasklistParserTest : GtkTest
+	public class DeserializeTests : GtkTest
 	{
 		
 		private Note singleTaskListNote;
@@ -53,6 +56,10 @@ namespace Tomboy.TaskManager.Tests
 			TestNotesManager.CreateNote("DoubleTaskListNote", out doubleTaskListNote, out doubleManager);
 		}
 
+		
+		/// <summary>
+		/// Checks if the parser correctly finds a tasklist and all its tasks contained.
+		/// </summary>
 		[Test()]
 		public void FindTaskList ()
 		{
@@ -61,6 +68,10 @@ namespace Tomboy.TaskManager.Tests
 			Assert.That(tasklists[0].Children.Count == 3); // with 3 tasks
 		}
 
+		
+		/// <summary>
+		/// Checks if the parser correctly finds 2 tasklists and all its tasks contained.
+		/// </summary>
 		[Test()]
 		public void FindMultipleTaskLists ()
 		{
@@ -68,6 +79,16 @@ namespace Tomboy.TaskManager.Tests
 			Assert.That(tasklists.Count == 2); // two task lists are found
 			Assert.That(tasklists[0].Children.Count == 3); // 1st tasklist has 3 tasks
 			Assert.That(tasklists[1].Children.Count == 2); // 2nd tasklist has 2 tasks
+		}
+		
+		
+		/// <summary>
+		/// This Test ensures that priorities are read correctly from the XML file.
+		/// </summary>
+		[Test()]
+		public void LoadingPriorities ()
+		{
+			
 		}
 		
 		
